ff15297047057e3ef71d904f2e254aee804c27be
Matthew Holloway <matthew.holloway@logicwerx.com>
2015-04-22 20:36:10 -0700

    Add computed level and reset

--- js/app.js
+++ js/app.js
@@ -3,9 +3,22 @@ var ViewModel = function() {
   this.name = ko.observable('Tabby');
   this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
   this.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
+  this.level = ko.computed(function() {
+    if (this.clickCount() >= 0 && this.clickCount() <=9) {
+      return "Newborn";
+    } else if (this.clickCount() >= 10 && this.clickCount() <= 19) {
+      return "Infant";
+    } else {
+      return "Full Grown";
+    }
+  }, this);
 
   this.incrementCounter = function() {
     this.clickCount(this.clickCount() + 1);
+  };
+
+  this.resetCounter = function() {
+    this.clickCount(0);
   };
 }
 
